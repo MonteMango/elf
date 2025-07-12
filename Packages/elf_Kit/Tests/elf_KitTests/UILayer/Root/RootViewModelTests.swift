4ef379575f0e0142041f46daea02a5e7d19f0acb
Vitalii Lytvynov <vitalii.lytvynov@gmail.com>
2025-07-12 15:32:07 +0200

    List of changes:  - removed thread chack for rooViewModel

--- Packages/elf_Kit/Tests/elf_KitTests/UILayer/Root/RootViewModelTests.swift
+++ Packages/elf_Kit/Tests/elf_KitTests/UILayer/Root/RootViewModelTests.swift
@@ -35,62 +35,20 @@ final class RootViewModelTests: XCTestCase {
         viewModel.setViewState(.battle)
         XCTAssertEqual(viewModel.viewState, .battle)
     }
-
-    func testLoadHeroItemsCallsRepositoryOffMainThread() async throws {
-        viewModel.loadHeroItems()
-
-        try await XCTAssertEventually(self.fakeRepository.didCallLoadHeroItems)
-
-        XCTAssertFalse(
-            fakeRepository.wasCalledOnMainThread,
-            "Метод loadHeroItems() должен вызываться не на главном потоке"
-        )
-    }
 }
 
 final class FakeItemsRepository: ItemsRepository {
     var heroItems: HeroItems? = nil
-    
-    private(set) var didCallLoadHeroItems = false
-    private(set) var wasCalledOnMainThread = false
     
     var heroItemsPublisher: AnyPublisher<HeroItems?, Never> {
         Just(heroItems).eraseToAnyPublisher()
     }
 
     func loadHeroItems() async throws {
-        try await Task.sleep(nanoseconds: 50_000_000) // Эмуляция задержки
-        didCallLoadHeroItems = true
-        wasCalledOnMainThread = DispatchQueue.isMain
+        return // можно дополнить, если понадобится
     }
 
     func getHeroItem(_ id: UUID) async -> Item? {
         return nil // можно дополнить, если понадобится
-    }
-}
-
-func XCTAssertEventually(
-    _ expression: @autoclosure @escaping () -> Bool,
-    timeout: TimeInterval = 1.0,
-    interval: TimeInterval = 0.01,
-    file: StaticString = #file,
-    line: UInt = #line
-) async throws {
-    let start = Date()
-    while Date().timeIntervalSince(start) < timeout {
-        if expression() {
-            return
-        }
-        try await Task.sleep(nanoseconds: UInt64(interval * 1_000_000_000))
-    }
-    XCTFail("Условие не выполнено за \(timeout) секунд", file: file, line: line)
-}
-
-extension DispatchQueue {
-    static var isMain: Bool {
-        let key = DispatchSpecificKey<String>()
-        let value = "main"
-        DispatchQueue.main.setSpecific(key: key, value: value)
-        return DispatchQueue.getSpecific(key: key) == value
     }
 }
